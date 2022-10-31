e54061e7f21a2faa15fe4ab15eb7f8c94c93e460
rselimp <rselimp@gmail.com>
2022-10-31 16:53:46 +0600

    register added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
     <div className="text-center lg:text-left">
       <img className='w-3/4' src={img} alt="" />
     </div>
-    <div className="w-full max-w-sm shadow-2xl card bg-base-100 my-10 py-10">
+    <div className="w-full max-w-sm shadow-2xl card bg-base-100 py-10">
     <h1 className="text-5xl font-bold text-center">Login now!</h1>
       <form onSubmit={handleSubmit} className="card-body">
         <div className="form-control">
